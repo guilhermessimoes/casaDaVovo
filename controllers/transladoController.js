f1793969c116b9f153da3eb75d9fc4711b805348
Seu nome para exibição <guilhermejunios@gmail.com>
2021-12-02 20:14:35 -0300

    Subindo alterações

--- controllers/transladoController.js
+++ controllers/transladoController.js
@@ -3,22 +3,77 @@ const db = require('../models')
 
 const transladoController = {
     listagemTranslado: async(req,res)=>{
-        res.render('listagemTranslado', {formAction:"/listagemTranslado", usuario:{}})
+        const cadastroTransladoRows = await db.Translado.findAll();   
+       // const messages = await req.consumeFlash('success')
+        res.render('listagemTranslado', {translados: cadastroTransladoRows})
     },
 
     viewCadastrarTranslado: async(req,res)=>{
-        res.render('cadastrarTranslado', {formAction:"/cadastrarTranslado", usuario:{}})
+        res.render('cadastrarTranslado', {formAction:"cadastrarTranslado", translado:{}})
     },
 
     acaoCadastrarTranslado: async(req,res) =>{
-        const nomeUsuario = req.body.nome;
-        const emailUsuario = req.body.email;
-    
-        await db.Usuario.create({
-            nome: nomeUsuario,
-            email: emailUsuario
+        const transladoPet = req.body.transportaPet
+        const transladoDeficiente = req.body.acessivelDeficiente
+        const transladoBagagens = req.body.levarBagagens
+        const transladoRecolhe = req.body.recolheHotel
+        const transladoDescricao = req.body.descricao
+        const transladoImagem = req.file.filename
+        const transladoPrecoOriginal = req.body.precoOriginalTranslado
+        const transladoPrecoPromocional = req.body.precoPromocionalTranslado
+        const transladoTitulo = req.body.tituloTranslado
+
+        await db.Translado.create({
+            transportaPet: transladoPet,
+            acessivelDeficiente: transladoDeficiente,
+            levarBagagens: transladoBagagens,
+            recolheHotel: transladoRecolhe,
+            descricao: transladoDescricao,
+            imagemTranslado: transladoImagem,
+            precoOriginalTranslado: transladoPrecoOriginal,
+            precoPromocionalTranslado: transladoPrecoPromocional,
+            tituloTranslado: transladoTitulo
         })
-        res.redirect("/listagemTranslado")     
+
+        //await req.flash('success', "Registro criado com sucesso")
+
+        res.redirect("/translado/listagemTranslado")   
+    },
+
+    acaoEditar: async (req,res) =>{
+        let listaDeErros = validationResult(req)
+        if(!listaDeErros.isEmpty()){
+            const transladoEncontrado = await db.Translado.findByPk(req.params.id);          
+            const alert = listaDeErros.array()
+            console.log(alert)
+
+            res.render("cadastrarTranslado", {alert: alert, formAction:`/alterar/${req.params.id}`,
+            buttonMessage: "Salvar", translado: transladoEncontrado})
+            return            
+        }
+
+        const transladoObj = { 
+            transportaPet: transladoPet,
+            acessivelDeficiente: transladoDeficiente,
+            levarBagagens: transladoBagagens,
+            recolheHotel: transladoRecolhe,
+            descricao: transladoDescricao,
+            filename: transladoImagem
+        }
+
+        await db.Translado.update(transladoObj, {where: {id: req.params.id}})
+
+        //await req.flash('success', "Registro editado com sucesso")
+
+        res.redirect("/")
+    },
+
+    excluir: async (req, res)=> {
+        const idTranslado = req.params.id;
+        
+        await req.flash('success', "Registro excluido com sucesso")
+        await db.Translado.destroy({where: {id: idTranslado}})
+        res.redirect("/")
     }
 }
 
--- routes/translado.js
+++ routes/translado.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const transladoController = require('../controllers/transladoController')
+const path = require('path')
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, res, cb) =>{
+        cb(null, "./public/data/uploads")
+    },
+    filename:  (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage })
 
 
 
-
+router.get('/cadastrarTranslado', transladoController.viewCadastrarTranslado)
+router.post('/cadastrarTranslado', upload.single('imagemTranslado'), transladoController.acaoCadastrarTranslado)
 router.get('/listagemTranslado', transladoController.listagemTranslado)
 
 
